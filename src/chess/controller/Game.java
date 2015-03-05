32802d772c1e37ccc346235823cc703acb2101e8
Cassio Sousa <cassiosantossousa@gmail.com>
2015-03-05 04:28:25 -0600

    Organized Game to be easy to test

--- src/chess/controller/Game.java
+++ src/chess/controller/Game.java
@@ -3,6 +3,8 @@ package chess.controller;
 import chess.model.board.ChessBoard;
 import chess.view.gui.ChessInterface;
 
+import java.util.HashMap;
+
 /**
  * <strong>Game</strong> class - implements the actual game of Chess.
  *
@@ -13,6 +15,20 @@ public class Game {
 
     private static final String PIECE_IN_CHECK = "King";
     private static ChessBoard gameBoard;
+    private static ChessInterface chessGui;
+    private int plays_counter;
+    private HashMap<Integer, ChessBoard> previous_games;
+
+    /**
+     * Starts a new game.
+     */
+    public Game(){
+        previous_games = new HashMap<Integer, ChessBoard>();
+        plays_counter = 0;
+        gameBoard = new ChessBoard();
+        gameBoard.setupPieces();
+        chessGui = new ChessInterface(gameBoard);
+    }
 
     /**
      * Main function that draws a new chessboard.
@@ -20,9 +36,7 @@ public class Game {
      * @param args String entries - none are required.
      */
     public static void main(String[] args) {
-        gameBoard = new ChessBoard();
-        gameBoard.setupPieces();
-        new ChessInterface(gameBoard);
+        new Game();
     }
 
 
