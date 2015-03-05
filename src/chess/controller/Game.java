0ed7ea81c5c5cacd85eb848dcce8912968f8d10b
Cassio Sousa <cassiosantossousa@gmail.com>
2015-03-04 23:53:21 -0600

    Created Game on controller

---
+++ src/chess/controller/Game.java
@@ -0,0 +1,11 @@
+package chess.controller;
+
+/**
+ * <strong>Game</strong> class - implements the actual game of Chess.
+ *
+ * @author Cassio dos Santos Sousa
+ * @version 1.0
+ */
+
+public class Game {
+}
